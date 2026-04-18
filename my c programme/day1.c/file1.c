c76ab6edfd252a5c167ee63a631b2e582d759a5c
Subhasharaddy <subhasharaddy497@gmail.com>
2026-04-18 15:23:34 +0530

    Add files via upload

---
+++ my c programme/day1.c/file1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int integer;
+    float f;
+    printf("enter a integer: ");
+    scanf("%d",&integer);
+    printf("you enterd %d as ainteger",integer);
+    printf("\nenter a floating point number:  ");
+    scanf("%f",&f);
+    printf("you enterd %f as a floating point numer",f);
+
+    return 0;
+}
---
+++ my c programme/day1.c/file2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char string[20];
+    printf("enter a string: ");
+    scanf("%c",&string);
+    printf("you enterd %s as a string\n",string);
+    //system("pause");
+    char c; 
+    printf("\nEnter a character: ");
+    scanf("%c",&c);
+    p
+    getchar();
+    c =getchar();
+    printf("You entered: %c\n", c);
+    return 0;
+}
